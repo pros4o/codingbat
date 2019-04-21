e05c7e04ab67dfb1e8673929fcd3e37f79a5adff
pros4o <Easy8r99ze@yandex.ru>
2019-04-22 00:12:44 +0300

    Add some task to ArrayOne

--- src/com/test/app/java/ArrayOne.java
+++ src/com/test/app/java/ArrayOne.java
@@ -35,15 +35,91 @@ public class ArrayOne {
     }
     
     //6 task
+    public int[] rotateLeft3(int[] nums) {
+      int temp = nums[0];
+      for (int i = 0; i < nums.length - 1; i++) {
+        nums[i] = nums[i+1];
+      }
+      nums[nums.length - 1] = temp;
+      return nums;
+    }
+
     //7 task
+    public int[] reverse3(int[] nums) {
+      int temp = nums[0];
+      nums[0] = nums[nums.length-1];
+      nums[nums.length-1] = temp;
+      return nums;
+    }
+
     //8 task
+    public int[] maxEnd3(int[] nums) {
+      if (nums[0]>nums[nums.length-1]) {
+        for(int i=0;i<nums.length;i++)
+          nums[i]=nums[0];
+        return nums;
+      }
+      for(int i=0;i<nums.length;i++)
+          nums[i]=nums[nums.length-1];
+        return nums;
+    }
+
     //9 task
+    public int sum2(int[] nums) {
+      if(nums.length==0) return 0;
+  
+    if(nums.length<2) return nums[0];
+  
+      return nums[0]+nums[1];
+    }
+  
     //10 task
+    public int[] middleWay(int[] a, int[] b) {
+      int[] c = new int [2];
+      c[0] = a[1];
+      c[1] = b[1];
+      return c;
+    }
+    
     //11 task
+    public int[] makeEnds(int[] nums) {
+      int[] some = new int [2];
+      some[0] = nums[0];
+      some[1] = nums[nums.length - 1];
+      return some;
+    }
+    
     //12 task
+    public boolean has23(int[] nums) {
+      for (int i = 0; i < nums.length; i++)
+        if(nums[i] == 2 || nums[i] == 3) return true;
+      return false;
+    }
+
     //13 task
+    public boolean no23(int[] nums) {
+      for (int i = 0; i < nums.length; i++)
+      if(nums[i] == 2 || nums[i] == 3) return false;
+      return true;
+    }
+
     //14 task
+    public int[] makeLast(int[] nums) {
+      int[] some = new int[nums.length * 2];
+      for (int i = 0; i < some.length - 1; i++)
+        some[i] = 0;
+      some[some.length - 1] = nums[nums.length - 1];
+      return some;
+    }
+    
     //15 task
+    public boolean double23(int[] nums) {
+      for (int i = 0; i < nums.length - 1; i++)
+        if((nums[i] == 2 || nums[i] == 3) && (nums[i+1] == nums[i])) 
+          return true;  
+      return false;
+    }
+
     //16 task
     //17 task
     //18 task
@@ -56,4 +132,24 @@ public class ArrayOne {
     //25 task
     //26 task
     //27 task
+    public int[] front11(int[] a, int[] b) {
+      int[] c = new int[1];
+   
+   if (b.length==0 && a.length==0) return a;
+  
+    if(a.length==0) {c[0]=b[0]; return c;}
+  
+    if(b.length==0) {c[0]=a[0]; return c;}
+   
+   
+    int[] w = new int[2];
+  
+    w[0]=a[0];
+    
+  w[1]=b[0];
+  
+    
+    
+    return w;
+  }
 }

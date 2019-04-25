97f3c4dc49159a73aefc66dcb58c6f81e6db378d
pros4o <Easy8r99ze@yandex.ru>
2019-04-26 01:09:57 +0300

    Add 1-3 String-1

---
+++ src/com/test/app/java/StringOne.java
@@ -0,0 +1,21 @@
+package com.test.app.java;
+
+public class StringOne {
+
+    //1 task
+    public String helloName(String name) {
+        return "Hello "+name+"!";
+    }
+
+    //2 task
+    public String makeAbba(String a, String b) {
+        return a+b+b+a;
+    }
+
+    //3 task
+    public String makeTags(String tag, String word) {
+        return "<"+tag+">"+word+"</"+tag+">";
+    }
+
+    //4 task
+}

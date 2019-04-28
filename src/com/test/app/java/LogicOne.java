03963c6dc7d505a06c950d932e79b06131f9134f
pros4o <Easy8r99ze@yandex.ru>
2019-04-28 21:40:37 +0300

    Logic1 task 1-2

---
+++ src/com/test/app/java/LogicOne.java
@@ -0,0 +1,43 @@
+package com.test.app.java;
+
+public class LogicOne {
+    //1 task
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if((cigars >= 40 && cigars <= 60) || (isWeekend && cigars >= 40)) return true;
+        return false;
+    }
+
+    //2 task
+    public int dateFashion(int you, int date) {
+        if ((you >= 8 || date >= 8) && (you != 2 && date != 2)) return 2;
+        else if (you>2 && date>2) return 1;
+        else return 0;
+    }
+      
+    //3 task
+
+    //4 task
+
+    //5 task
+    //6
+    //7
+    //8
+    //9
+    //10
+    //11
+    //12
+    //13
+    //14
+    //15
+    //16
+    //17
+    //18
+    //19
+    //20
+    //21
+    //22
+    //23
+    //24
+    //25
+    //26
+}
